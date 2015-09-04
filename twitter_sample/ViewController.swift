baad06337d534f58740ff255226e6318bc74b9a9
hiro.nagami <hiro.nagami@gmail.com>
2015-09-05 01:56:51 +0900

    update source

--- twitter_sample/ViewController.swift
+++ twitter_sample/ViewController.swift
@@ -12,7 +12,8 @@ import Accounts
 class ViewController: UIViewController {
     
     var accountStore = ACAccountStore()   // 追加
-    var twAccount = ACAccount?()          // 追加
+    var twAccount = ACAccount()          // 追加
+    var accounts = [ACAccount]()
 
     @IBOutlet var loginButton: UIButton!
 
@@ -25,51 +26,61 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tappedLoginButton(sender: AnyObject) {
-        selectTwitterAccountsFromDevice()
+        getTwitterAccountsFromDevice()
     }
     
-    /* iPhoneに設定したTwitterアカウントの選択画面を表示する */
-    func selectTwitterAccountsFromDevice(){
+    /* iPhoneに設定したTwitterアカウントの情報を取得する */
+    func getTwitterAccountsFromDevice(){
         let accountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
         accountStore.requestAccessToAccountsWithType(accountType, options: nil) { (granted:Bool, aError:NSError?) -> Void in
             
+            // アカウント取得に失敗したとき
             if let error = aError {
                 println("Error! - \(error)")
                 return;
             }
             
+            // アカウント情報へのアクセス権限がない時
             if !granted {
                 println("Cannot access to account data")
                 return;
             }
             
+            // アカウント情報の取得に成功
             let accounts = self.accountStore.accountsWithAccountType(accountType) as! [ACAccount]
-            self.showAndSelectTwitterAccountWithSelectionSheets(accounts)
+            self.showAndSelectTwitterAccountWithSelectionSheets()
         }
     }
     
-    /* アカウントを選択したあと、タイムライン画面へ遷移する処理 */
-    func showAndSelectTwitterAccountWithSelectionSheets(accounts: [ACAccount]) {
+    /* iPhoneに設定したTwitterアカウントの選択画面を表示する */
+    func showAndSelectTwitterAccountWithSelectionSheets() {
         
+        // アクションシートの設定
         var alertController = UIAlertController(title: "Select Account", message: "Please select twitter account", preferredStyle: .ActionSheet)
         
         for account in accounts {
+            
             alertController.addAction(UIAlertAction(title: account.username, style: .Default, handler: { (action) -> Void in
+                // 選択したアカウントをtwAccountに保存
                 self.twAccount = account
                 self.performSegueWithIdentifier("segueTimelineViewController", sender: nil)
             }))
+            
         }
         
+        // キャンセルボタンは何もせずにアクションシートを閉じる
         let CanceledAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
         alertController.addAction(CanceledAction)
 
+        // アクションシート表示
         self.presentViewController(alertController, animated: true, completion: nil)
     }
     
+    // TimelineViewControllerを表示する際に選択したアカウントを渡す
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "segueTimelineViewController" {
             var vc = segue.destinationViewController as! TimelineViewController
-            vc.twAccount = self.twAccount!
+            vc.twAccount = self.twAccount
         }
     }
     
